0d299e7cdc735cb697d29e412e0ffb2428aeb529
jhchoo-beep <jh.choo@mgrv.company>
2026-03-17 13:13:02 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -386,7 +386,7 @@ export default function Home() {
         <title>OTA 리뷰 대시보드</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Pretendard:wght@400;500;600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;600&display=swap" rel="stylesheet" />
       </Head>
 
       <div className="app">
